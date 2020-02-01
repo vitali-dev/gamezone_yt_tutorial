5f5c5caf5816a0120861fdc7e14ff35dc5a0a378
vitali-dev <vitali.repin@gmail.com>
2020-02-01 18:42:16 +0100

    Added Formik Forms

--- screens/home.js
+++ screens/home.js
@@ -5,11 +5,15 @@ import {
   Text,
   FlatList,
   TouchableOpacity,
+  Modal,
 } from 'react-native';
 import { globalStyles } from '../styles/global';
 import Card from '../shared/card';
+import { MaterialIcons } from '@expo/vector-icons';
+import ReviewForm from './reviewForm';
 
 export default function Home({ navigation }) {
+  const [modalOpen, setModalOpen] = useState(false);
   const [reviews, setReviews] = useState([
     {
       title: 'Zelda, Breath of Gresh Air',
@@ -31,8 +35,38 @@ export default function Home({ navigation }) {
     },
   ]);
 
+  const addReview = review => {
+    review.key = Math.random().toString();
+    setReviews(currentReviews => {
+      return [review, ...currentReviews];
+    });
+    setModalOpen(false);
+  };
+
   return (
     <View style={globalStyles.container}>
+      <Modal
+        visible={modalOpen}
+        animationType='slide'
+        presentationStyle='formSheet'
+      >
+        <View style={styles.modalContent}>
+          <MaterialIcons
+            name='close'
+            size={24}
+            style={{ ...styles.modalToggle, ...styles.modalClose }}
+            onPress={() => setModalOpen(false)}
+          />
+          <ReviewForm addReview={addReview} />
+        </View>
+      </Modal>
+
+      <MaterialIcons
+        name='add'
+        size={24}
+        style={styles.modalToggle}
+        onPress={() => setModalOpen(true)}
+      />
       <FlatList
         data={reviews}
         renderItem={({ item }) => (
@@ -48,3 +82,21 @@ export default function Home({ navigation }) {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  modalContent: {
+    flex: 1,
+  },
+  modalToggle: {
+    marginBottom: 10,
+    borderWidth: 1,
+    borderColor: '#f2f2f2',
+    padding: 10,
+    borderRadius: 1,
+    alignSelf: 'center',
+  },
+  modalClose: {
+    marginTop: 35,
+    marginBottom: 0,
+  },
+});
